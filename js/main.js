7f745c995938ea2945b5c1a9dbfed5ad67b45d80
user-amantai <127530754+user-amantai@users.noreply.github.com>
2023-05-21 18:37:28 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -180,3 +180,18 @@
     });
 
 })(jQuery);
+
+// anchors
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for(let anchor of anchors){
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault()
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: 'start'
+        })
+    })
+}
